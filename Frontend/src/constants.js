f54c649556acfff3887de0e66ea1e3bc96551d89
Deepak Rawat <deepak.rawat@changecx.com>
2024-01-21 19:17:57 +0530

    update backend url

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,4 +1,4 @@
 export const APP_NAME = "Merge";
 export const APP_URL = "https://alanbinu-video-conference.web.app";
-export const BACKEND_URL = "http://localhost:9000";
+export const BACKEND_URL = "https://doctor-api-wpn8.onrender.com";
 // export const BACKEND_URL='http://localhost:5000';
