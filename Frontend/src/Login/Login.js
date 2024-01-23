0ae89129cc49371b4ca4376fb30f4d5c64119d2a
jagadeeswar-N-G <jagadeeswar8055@gmail.com>
2024-01-23 22:49:03 +0530

    Log in page responsive

--- Frontend/src/Login/Login.js
+++ Frontend/src/Login/Login.js
@@ -53,11 +53,11 @@ const Login = (props) => {
   };
 
   return (
-    <section class="bg-gray-50 dark:bg-gray-900 py-10 my-10">
-      <div class="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
+    <section class="bg-[#F2F4EA]">
+      <div class="flex flex-col items-center justify-center px-6 h-screen">
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">
               Login
             </h1>
             <form class="space-y-4 md:space-y-6">
@@ -76,7 +76,7 @@ const Login = (props) => {
                   }}
                   name="username"
                   id="username"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-green-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 "
                   placeholder="name@company.com / 1234567890"
                   required="true"
                 />
@@ -97,13 +97,13 @@ const Login = (props) => {
                   }}
                   id="password"
                   placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-green-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 "
                   required=""
                 />
               </div>
 
               <button
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full text-white bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-md px-5 py-2.5 text-center"
                 // onClick={() => {
                 //   props.history.push("doctors");
                 // }}
@@ -111,11 +111,11 @@ const Login = (props) => {
               >
                 Login
               </button>
-              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p class="text-sm font-light text-gray-500 text-center">
                 Dont have an account?{" "}
                 <Link
                   to="/"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium text-green-600 hover:underline"
                 >
                   Signup here
                 </Link>
