58a7257216ba8158ebb75b87f37f50b5260b023f
AshwinSingh0807 <ashwin.singh@changecx.com>
2024-01-24 13:07:51 +0530

    Ashwin-header-login

--- Frontend/src/Components/Header/Menu.js
+++ Frontend/src/Components/Header/Menu.js
@@ -11,6 +11,10 @@ const Menu = () => {
     }
   }, []);
 
+  const handleLogOut = () => {
+    localStorage.clear();
+  }
+
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
   };
@@ -57,12 +61,12 @@ const Menu = () => {
             </li>
           </ul>
           <div class="py-1">
-            <a
-              href="#"
+            <button
+              onClick={handleLogOut}
               class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
             >
               Sign out
-            </a>
+            </button>
           </div>
         </div>
       </div>
